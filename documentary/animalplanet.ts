ab7af1982f40bea6ea7dbf6a81b389ddae854487
Playidturl1 <100044137+Playidturl1@users.noreply.github.com>
2022-10-19 21:38:37 +0700

    Create animalplanet.ts

---
+++ documentary/animalplanet.ts
@@ -0,0 +1,4 @@
+#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2000000,RESOLUTION=1920x1080,NAME="1080p"
+https://rr5---sn-w5nuxa-c33lk-37.googleuservideo.com/doofootball_r7/hd-animalplanet/playlist.m3u8
