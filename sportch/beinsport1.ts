12cca0caf30684f0a0d203dfbee199e652c8c52c
Playidturl1 <100044137+Playidturl1@users.noreply.github.com>
2022-11-02 02:05:06 +0700

    Update beinsport1.ts

--- sportch/beinsport1.ts
+++ sportch/beinsport1.ts
@@ -1,12 +1,6 @@
 #EXTM3U
 #EXT-X-VERSION:3
-#EXT-X-TARGETDURATION:7
-#EXT-X-MEDIA-SEQUENCE:15055
-#EXTINF:5.999,
+#EXT-X-STREAM-INF:BANDWIDTH=4596000,CODECS="avc1.77.31,mp4a.40.2",RESOLUTION=1920x1080
 https://cdn.googlecdnforstreaming.live/dbf789/bein1/chunks.m3u8
-#EXTINF:6,
-https://cdn.googlecdnforstreaming.live/dbf789/bein1/chunks.m3u8
-#EXTINF:6.001,
-l_404902_30954597_15057.ts?nimblesessionid=59506640
-#EXTINF:6,
-l_404902_30960598_15058.ts?nimblesessionid=59506640
+#EXT-X-STREAM-INF:BANDWIDTH=2496000,CODECS="avc1.77.30,mp4a.40.2",RESOLUTION=1280x720
+https://cdn.googlecdnforstreaming.live/dbf789/bein1/playlist.m3u8
