78fc3bbdcabb1faf2c01dcb3c5d140c822874467
playidtvwebpag <117137632+playidtvwebpag@users.noreply.github.com>
2022-11-03 04:09:27 +0700

    Update beinsport3.ts

--- sportch/beinsport3.ts
+++ sportch/beinsport3.ts
@@ -1,6 +1,4 @@
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:BANDWIDTH=4596000,CODECS="avc1.77.31,mp4a.40.2",RESOLUTION=1920x1080
-https://cdn.googlecdnforstreaming.live/dbf789/bein3/playlist.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=2496000,CODECS="avc1.77.30,mp4a.40.2",RESOLUTION=1280x720
-https://cdn.googlecdnforstreaming.live/dbf789/bein3/chunks.m3u8
+https://streaming.livescorethai.net/iptv/epl-bein3.stream/playlist.m3u8
